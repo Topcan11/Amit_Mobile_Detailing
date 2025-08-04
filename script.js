47e3d4c98fea35161248d3823725aa76ce47f0a1
Topcan11 <joshephtamang1111@gmail.com>
2025-08-04 14:00:49 -0700

    Added a script to scroll to the top

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 document.getElementById("close-popup").addEventListener("click", function() {
   document.getElementById("popup").style.display = "none";
 });
+
+    // Show/hide the scroll-to-top button
+    window.onscroll = function() {
+      var btn = document.getElementById("toTopBtn");
+      if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+        btn.style.display = "block";
+      } else {
+        btn.style.display = "none";
+      }
+    };
+
+    // Scroll to top functionality
+    document.getElementById("toTopBtn").onclick = function() {
+      window.scrollTo({top: 0, behavior: 'smooth'});
+    };
